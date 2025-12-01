2f95444f16ef55c32f7cfe67298bdcc7edea18fe
David Monteiro <dmonn97@gmail.com>
2025-12-01 02:45:22 -0300

    JS Fundamentos - String e Template String

---
+++ 1_modulo_primeiros_passos/12_string_e_template_string/assets/js/jscript.js
@@ -0,0 +1,9 @@
+let nome = "David";
+let sobrenome = "Monteiro";
+//let fullName = nome + " " + sobrenome;
+let fullName = `${nome} ${sobrenome}`;
+let idade = 28;
+let idadeString = `${idade + 2} anos`;
+
+console.log(fullName);
+console.log(idadeString);
