5d2520bda1dabaed6e5324dde32ce66fb4fc55c9
David Monteiro <dmonn97@gmail.com>
2025-12-03 04:55:55 -0300

    JS Fundamentos - Att e prox aula

---
+++ 1_modulo_primeiros_passos/16_condicional_dupla_(if_else)/assets/js/jscript.js
@@ -0,0 +1,20 @@
+let idade = 20;
+/*
+if(idade >= 18) {
+    if(idade < 60) {
+        console.log(`Voce e adulto`)
+    }
+}
+*/
+
+// Condicional &&
+/*
+if(idade >= 18 && idade < 60) {
+    console.log(`Voce e um adulto`)
+}
+*/
+
+//Condicional ||
+if(idade >= 18 || idade < 60) {
+    console.log(`Voce e um adulto`)
+}
