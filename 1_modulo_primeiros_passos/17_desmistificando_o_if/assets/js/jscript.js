4d10e7deff6ae9e6bd2c1aa52119180e4873efb2
David Monteiro <dmonn97@gmail.com>
2025-12-05 02:58:40 -0300

    JS Fundamentos - Desmistificando o If

---
+++ 1_modulo_primeiros_passos/17_desmistificando_o_if/assets/js/jscript.js
@@ -0,0 +1,13 @@
+let idade = 28;
+
+// Exemplo de Condicional em uma variavel
+let verificacao = idade > 18 && idade < 60;
+
+if(idade >18 && idade <  60) {
+    console.log(`Passou na verificacao`);
+} else {
+    console.log(`Nao passou na Verificacao.`)
+}
+
+console.log(verificacao);
+
