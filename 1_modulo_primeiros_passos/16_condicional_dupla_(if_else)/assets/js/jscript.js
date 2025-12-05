83c9e5aa35b26e347009b0fa9d97aa526d268afa
David Monteiro <dmonn97@gmail.com>
2025-12-05 01:09:27 -0300

    JS Fundamentos - Condicional Dupla (If Else)

--- 1_modulo_primeiros_passos/16_condicional_dupla_(if_else)/assets/js/jscript.js
+++ 1_modulo_primeiros_passos/16_condicional_dupla_(if_else)/assets/js/jscript.js
@@ -1,20 +1,27 @@
-let idade = 20;
-/*
-if(idade >= 18) {
-    if(idade < 60) {
-        console.log(`Voce e adulto`)
-    }
-}
-*/
+let idade = 17;
 
-// Condicional &&
+
+// EXEMPLO DE CONDICIONAL DEPENDENTE
 /*
+if(idade < 18) {
+    console.log("Voce e uma crianca, idade informada:" . idade);
+}
+
 if(idade >= 18 && idade < 60) {
-    console.log(`Voce e um adulto`)
+    console.log(`Voce e um adulto, idade informada: ${idade}`);
+}
+
+if(idade > 60) {
+    console.log(`Voce e um idoso, idade informada: ${idade}`);
 }
 */
 
-//Condicional ||
-if(idade >= 18 || idade < 60) {
-    console.log(`Voce e um adulto`)
+// EXEMPLO DE CONDICIONAL DEPENDENTE
+if(idade < 18) {
+    console.log("Você é uma criança. Idade informada: " + idade);
+} else if(idade >= 18 && idade < 60) {
+    console.log(`Você é um adulto. Idade informada: ${idade}`);
+} else if(idade >= 60) {
+    console.log(`Você é um idoso. Idade informada: ${idade}`)
 }
+
