33a3a7dfaddf4ed12d3ef712c8dc8ea4e0043498
David Monteiro <dmonn97@gmail.com>
2025-12-07 03:38:14 -0300

    JS Basico - Declarando uma Funcao

--- 02_js_basico/02_declarando_uma_funcao/assets/js/jscript.js
+++ 02_js_basico/02_declarando_uma_funcao/assets/js/jscript.js
@@ -1,16 +1,4 @@
-let profession = "vendedor";
-
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
-
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+function gravidade() {
+    console.log(`A gravidade do planeta é: `);
+    console.log(9.8);
 }
