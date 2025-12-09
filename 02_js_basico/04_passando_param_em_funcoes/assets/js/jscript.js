d44f340cc1262de9cc8c9f65e0d993c7f73d1311
David Monteiro <dmonn97@gmail.com>
2025-12-09 01:40:54 -0300

    JS Basico - Passando Parametros em Funcoes

--- 02_js_basico/04_passando_param_em_funcoes/assets/js/jscript.js
+++ 02_js_basico/04_passando_param_em_funcoes/assets/js/jscript.js
@@ -1,16 +1,13 @@
-let profession = "vendedor";
-
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
-
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+function somar(n1, n2) {
+    let resultado = n1 + n2
+    console.log(`Resultado: ${resultado}`);
 }
+
+somar(10,15);
+
+// Exemplo 2
+function fullName(name, lastName) {
+    console.log(`${name} ${lastName}`);
+}
+
+fullName("David", "Monteiro");
