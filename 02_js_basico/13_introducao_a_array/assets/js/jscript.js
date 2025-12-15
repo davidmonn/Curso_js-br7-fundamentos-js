9ffec2c48880b1414f702c04840099ad717d1703
David Monteiro <dmonn97@gmail.com>
2025-12-15 05:52:53 -0300

    JS Basico - Introducao a Array

--- 02_js_basico/13_introducao_a_array/assets/js/jscript.js
+++ 02_js_basico/13_introducao_a_array/assets/js/jscript.js
@@ -1,16 +1,37 @@
-let profession = "vendedor";
+let colors = ['Azul', 'Vermelho', 'Branco', 'Preto', 'Cinza', 'Green']
+let idades = [28, 25, 44, 69, 13];
+let pessoa = { //Nesse caso object unico
+    nome: 'David',
+    idade: 28,
+    profissao: 'Auxiliar de Producao',
+};
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+//Multiplos objects:
+let pessoas = [
+    {
+        nome: 'David',
+        profissao: 'Aux Producao, estudando programacao',
+        idade: 28 + ' Anos',
+    },
+    {
+        nome: 'Suzana',
+        profissao: 'Procurando como programadora',
+        idade: 25 + ' anos'
+    }
+];
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
-}
+
+console.log(colors[1]);
+console.log(idades[0]);
+
+//console.log(pessoa);
+console.log(pessoas[0].nome); // Para selecionar um [0] seleciona primeira lista e .nome o valor que esta no nome
+
+
+let nomes = ['David', 'Suzana', 'Toninha'];
+let sobNomes = ['Monteiro', 'Santos', 'Monteiro', nomes];
+
+let comidas = ['Frutas', ['banana', 'maracuja', 'maça', 'Manga'], 'Salgados', ['Pizza', 'Fogazza','Hamburguer','Esfiha']];
+
+console.log(comidas[1][2]); //comidas[1] → seleciona o array de frutas - [2] → seleciona "maça" dentro desse array
+// Resumindo:  O índice 1 acessa o array interno das frutas, e o índice 2 acessa o valor "maça" dentro desse array.
