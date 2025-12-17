f4b287d4b8b0bb6cfc92a0b6f77d1c920a65e8ab
David Monteiro <dmonn97@gmail.com>
2025-12-17 03:31:34 -0300

    JS Basico - Exercicio 04 - Arrray

--- 02_js_basico/15_exercicio_array_4/assets/js/jscript.js
+++ 02_js_basico/15_exercicio_array_4/assets/js/jscript.js
@@ -1,16 +1,40 @@
-let profession = "vendedor";
+/*
+01. No array abaixo, qual o númeroque pega a ferrari
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 
+console.log('1 ' + carros[x]);
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+02. Troque a Ferrari por Audi
+console.log('2. Lista com Audi:');
+console.log(carros);
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
-}
+03. Adicione Volvo a lista:
+console.log('3, Lista com Volvo');
+
+4. Exiba quantos itens tem no array
+console.log(4. Itens no array: );
+    
+*/
+
+// Ex 01
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1;
+
+console.log('1 ' + carros[x]);
+
+// Ex 02
+carros[1] = 'Audi';
+console.log('2. Lista com Audi: ' + carros);
+
+//Ex 03
+carros.push('Volvo');
+
+//Ex 04
+console.log(carros.length)
+
+
+//Quantidade de itens:
+// console.log(carros.length);
+
+//Mostrar itens da lista
+// console.log(carros);
