ec1c62a5fe73d5e6c92305cc18f90fbbdd5cbc35
David Monteiro <dmonn97@gmail.com>
2025-12-19 04:24:47 -0300

    JS Basico - Acessando e Alterando Objetos

--- 02_js_basico/17_acessando_e_alterando_objetos/assets/js/jscript.js
+++ 02_js_basico/17_acessando_e_alterando_objetos/assets/js/jscript.js
@@ -1,16 +1,33 @@
-let profession = "vendedor";
+let user = {
+    nome: "David",
+    idade: 28,
+    hobbies: ['Tocar Violao', 'Estudar Programacao', 'Jogar'],
+    faculdade: 'Analise e Desenvolvimento de Sistemas'
+};
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+console.log(user.nome, user.idade, user.hobbies);
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+//Alterando ou Acessando Objeto:
+user.nome = 'Suzana';
+user.idade = 25;
+//Atribuicao direta
+user.hobbies = ['Tocar Guitarra', 'Estudar Programacao', 'Jogar'];
+
+//Adicionar:
+user.hobbies.push('Assistir Series');
+
+console.log(user.nome, user.idade, user.hobbies);
+
+// Exemplo 02
+let personagem = {
+    nome: 'DaMon',
+    idade: 28,
+    consoles: [
+        {console: 'PS5', modelo: 'Slim - Padrao'},
+        {console: 'Nintendo Switch', modelo: 'Padrao'},
+        {pc: 'Dell G15', modelo: 'Intel rtx3050'}
+    ]
 }
+
+//Acessando Objeto com Array e Objeto:
+console.log(personagem.consoles[0].console);
