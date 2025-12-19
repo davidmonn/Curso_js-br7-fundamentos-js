2a445ad5c494ea9844024e2ba85718241f5c3cfb
David Monteiro <dmonn97@gmail.com>
2025-12-19 00:57:51 -0300

    JS Basico - Oque sao Objetos

--- 02_js_basico/16_oq_sao_objetos/assets/js/jscript.js
+++ 02_js_basico/16_oq_sao_objetos/assets/js/jscript.js
@@ -1,16 +1,41 @@
-let profession = "vendedor";
+/* OBJETO MULTIPLO
+let usuario = [
+    {
+        nome: 'David',
+        idade: 28, 
+        profissao: 'Auxiliar de Producao - Procurando oportunidades como dev',
+        hobbies: 'Tocar violao, estudar programacao'
+    },
+    {
+        nome: 'Suzana',
+        idade: 25,
+        prrofissao: 'Buscando oportunidade como dev',
+        hobbies: 'Estudar programacao, tocar guitarra, jogar'
+    }
+];
+*/
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+let user = {
+    nome: 'David',
+    idade: 28, 
+    profissao: 'Auxiliar de Producao. Procurando oportunidades como dev',
+    hobbies: 'Tocar violao, estudar programacao'
+};
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+//  OBJETO DENTRO DE OBJETO:
+let personagem = {
+    nome: 'DMon',
+    oficio: 'Guerreiro',
+    olhos: ['castanho', 'preto'],
+    atributos: {
+        nivel: 20,
+        forca: 35,
+        stamina: 20,
+        magia: 10
+    }
 }
+
+//console.log(`O usuario ${user.nome} tem ${user.idade}, trabalha como ${user.profissao} e tem como hobbies ${user.hobbies}`);
+
+console.log(personagem.atributos.nivel);
+console.log(personagem.olhos[1]);
