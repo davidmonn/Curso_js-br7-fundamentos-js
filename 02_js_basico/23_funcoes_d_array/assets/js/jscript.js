f68e3c6a80e00ee81626633e5dbad1a4e7f09b18
David Monteiro <dmonn97@gmail.com>
2026-01-24 04:39:59 -0300

    Funcoes de Array

--- 02_js_basico/23_funcoes_d_array/assets/js/jscript.js
+++ 02_js_basico/23_funcoes_d_array/assets/js/jscript.js
@@ -1,16 +1,2 @@
-let profession = "vendedor";
+let fruits = ['Maca', 'Uva', 'Laranja', 'Banana'];
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
-
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
-}
