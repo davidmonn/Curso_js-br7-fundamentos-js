8850c82e995983da03cd2f8f9b19f0e2a6a286b2
David Monteiro <dmonn97@gmail.com>
2026-01-24 05:30:47 -0300

    Ordenacao de Array

--- 02_js_basico/24_ordenacao_d_array/assets/js/jscript.js
+++ 02_js_basico/24_ordenacao_d_array/assets/js/jscript.js
@@ -1,16 +1,32 @@
-let profession = "vendedor";
+// let fruits = ['Maca', 'Uva', 'Laranja', 'Banana'];
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+// //Sort do menor pro maior a-z
+// fruits.sort();
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
-}
+// //reverse inverte porem se usada junto do sort, geramos uma lista de z-a
+// fruits.reverse();
+
+// console.log(fruits);
+
+let cars = [
+    {brand: 'Fiat', year: 2022},
+    {brand: 'BMW', year: 2018},
+    {brand: 'Ferrari', year: 2020}
+];
+
+// cars.sort((a, b) => {
+//     if(a.year > b.year) {
+//         return 1;
+//     }  else if(a.year < b.year) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+// });
+
+cars.sort((a, b) => {
+    return b.year - a.year; 
+});
+
+
+console.log(cars);
