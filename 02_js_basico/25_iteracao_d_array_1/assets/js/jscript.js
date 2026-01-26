5e79a5b434154377753d7f176b2d20bb6847ba8e
David Monteiro <dmonn97@gmail.com>
2026-01-26 07:18:39 -0300

    Iteracao de Array

--- 02_js_basico/25_iteracao_d_array_1/assets/js/jscript.js
+++ 02_js_basico/25_iteracao_d_array_1/assets/js/jscript.js
@@ -1,16 +1,55 @@
-let profession = "vendedor";
+let fruits = ['Maca', 'Pera', 'Laranja', 'Kiwi', 'Morango', 'Banana', 'Manga', 'Uva'];
 
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
+// let fruitSelect = fruits.filter((item) => {
+//     if(item.length > 4) {
+//         return true;
+//     } else {
+//         false
+//     }
+// })
 
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+// OU
+
+//Metodo de melhor entendimento
+// let frutas = fruits.every((frutas) => {
+//     if(frutas.length > 3) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
+
+// if(frutas) {
+//     console.log(`Todas frutas tem mais que 3 letras.`)
+// } else {
+//     console.log(`Ha frutas com menos que 3 letras`)
+// }
+
+// //Resumido
+// let fruitSelect = fruits.filter((more) => more.length > 4);
+
+// console.log(`Todas True: ${fruits}`)
+// console.log(`Uma False: ${fruitSelect}`);
+
+
+
+
+
+let ok = fruits.some((algum) => algum.length > 3);
+
+if (ok) {
+    console.log(`Alguma(s) fruta(s) da nossa lista possui mais que 3 letra(s)`);
+} else {
+    console.log(`Nenhuma fruta da nossa lista possui mais que 3 letras`);
+}
+
+let buscar = fruits.includes('Maca');
+console.log(buscar);
+
+// Ou
+
+if(fruits.includes('Maca')) {
+    console.log(`Fruta selecionada foi encontrada.`);
+} else {
+    console.log(`Nao encontrado`);
 }
