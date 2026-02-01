1297bfb96e9b1752fc5ff412c2b6b7c8d55f2c5f
David Monteiro <dmonn97@gmail.com>
2026-02-01 05:19:52 -0300

    Eventos de Clique

--- 3_eventos_e_manipulacao_de_doom/04_eventos_de_clique/assets/js/jscript.js
+++ 3_eventos_e_manipulacao_de_doom/04_eventos_de_clique/assets/js/jscript.js
@@ -1,16 +1,10 @@
-let profession = "vendedor";
-
-switch(profession) {
-    case 'fiscal' : 
-        console.log(`Sua camisa sera verde. Profissao Escolhida: ${profession}`)
-    break;
-    case 'bombeiro' :
-        console.log(`Sua camisa sera vermelha. Profissao Escolhida: ${profession}`)
-    break;
-
-    case 'policial' :
-        console.log(`Sua camisa sera azul. Profissao Escolhida: ${profession}`)
-    break;
-    default:
-        console.log(`Sua camisa sera preta. Profissao Escolhida ${profession}`)
+function criou() {
+    alert("Usuario Registrado")
 }
+
+function clicou() {
+    alert('clicou');
+}
+
+let botao = document.querySelector(".botao.two");
+botao.addEventListener("click",clicou);
