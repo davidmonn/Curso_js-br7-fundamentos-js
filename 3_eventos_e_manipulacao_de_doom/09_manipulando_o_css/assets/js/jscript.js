eea77664eeeafe505538f6ff0cf3116c2caf46a3
David Monteiro <dmonn97@gmail.com>
2026-02-15 16:18:41 -0300

    Manipulando CSS

--- 3_eventos_e_manipulacao_de_doom/09_manipulando_o_css/assets/js/jscript.js
+++ 3_eventos_e_manipulacao_de_doom/09_manipulando_o_css/assets/js/jscript.js
@@ -1,13 +1,32 @@
 function selectClic() {
     const clicSelect = document.querySelector(".manipCss");
-    clicSelect.addEventListener("click", btnSelect);
+    clicSelect.addEventListener("click", clicou);
 }
 
-function btnSelect() {
-    const clic = document.querySelector(".testeManip");
-    clic.innerText = "Funcionando";
+// function action() {
+//     const clic = document.querySelector('#prim');
+//     const clicou = clic.innerHTML;
 
-    console.log(clic);
+//     console.log(clicou);
+// 
+
+let clicColor = false;
+
+function clicou() {
+    //Com isso ja ira selecionar nossa primera tag <li>
+    const li = document.querySelector('li');
+
+    li.style.fontSize = '20px';
+    li.style.transition = '0.5s';
+
+    if(clicColor == false) {
+        li.style.backgroundColor = '#00F'
+        clicColor = true;
+    } else {
+        li.style.backgroundColor = '#F00';
+        clicColor = false;
+    }
+
 }
 
 selectClic();
